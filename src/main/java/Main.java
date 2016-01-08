754f1697f1d8d9abb8ca62ed37dfaeaf7d38f472
Artem Guskov <tema92-62@yandex.ru>
2016-01-08 17:45:30 +0300

    initial commit.

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+/**
+ * @author Guskov Artem
+ *         on 08.01.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
